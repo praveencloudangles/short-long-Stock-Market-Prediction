90dd9ded013315ca0910edbab2c8213b35fa1fd7
praveencloudangles <112871831+praveencloudangles@users.noreply.github.com>
2024-03-11 17:00:06 +0530

    Update data_extraction.py

--- data_extraction.py
+++ data_extraction.py
@@ -5,5 +5,7 @@ import boto3
 def data_load():
     path = "https://stock-market-usecase.s3.amazonaws.com/train.csv"
     df = pd.read_csv(path)
+
+    print("rows------------", df.head)
     return df
 data_load()
